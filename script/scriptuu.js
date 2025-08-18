cf70070c6dce5aee598f05e8a97aefde021bf2f6
mtlbook <mariza127562@gmail.com>
2025-08-18 23:13:13 +0300

    Update scriptuu.js

--- script/scriptuu.js
+++ script/scriptuu.js
@@ -64,7 +64,7 @@ async function crawlNovel(startUrl) {
         const result = [];
 
         // Parallel download queue (5 at a time)
-        const queue = new PQueue({ concurrency: 5 });
+        const queue = new PQueue({ concurrency: 25 });
         let completed = 0;
 
         // Progress tracker (single-line updates)
